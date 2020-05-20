67edbfd83031814b7dafe759b45e564c13d4dae5
selimfurkantekin <sftekin25@gmail.com>
2020-05-20 05:03:55 +0300

    tests

--- models/seq2seq.py
+++ models/seq2seq.py
@@ -145,7 +145,7 @@ class Seq2Seq(nn.Module):
 
         # <start> is mapped to id=2
         dec_inputs = torch.ones(batch, dtype=torch.long) * 2
-        pred, hidden = self.decoder(dec_inputs, hidden, enc_out)
+        pred, hidden = self.decoder(dec_inputs, hidden, enc_out).to(self.device)
 
         outputs = [pred]
         for i in range(1, title_len):
